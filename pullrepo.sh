aa615f3d93129b0753c03bdf54b9a5f62e60f839
Tejas Vedagiri <tejasvedagiri1223@gmail.com>
2023-09-08 00:08:22 -0400

    Updated pathing

---
+++ pullrepo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@github.com:Tejasvedagiri/ErorStockProducer.git
+git clone git@github.com:Tejasvedagiri/ErorStockHarvester.git
+git clone git@github.com:Tejasvedagiri/ErorStockAPIServices.git
