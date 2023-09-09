3f699cf2609ad4aa69ff6789cc8179a4cdc668ba
Tejas Vedagiri <tejasvedagiri1223@gmail.com>
2023-09-09 12:51:33 -0400

    Updated compose

--- pullrepo.sh
+++ pullrepo.sh
@@ -3,3 +3,4 @@
 git clone git@github.com:Tejasvedagiri/ErorStockProducer.git
 git clone git@github.com:Tejasvedagiri/ErorStockHarvester.git
 git clone git@github.com:Tejasvedagiri/ErorStockAPIServices.git
+git clone git@github.com:Tejasvedagiri/ErorStockUI.git
